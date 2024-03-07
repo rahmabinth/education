1612ce46e40ffa245f613b7c34ba5ba08d91540c
Rahma Binth Mohammad <rahmabinth@gmail.com>
2024-03-07 00:04:28 -0500

    Download data

--- scripts/01-download_data.R
+++ scripts/01-download_data.R
@@ -13,15 +13,13 @@
 library(haven)
 
 #### Download data ####
-figure1_data <- read_dta("data/raw_data/Figure_1_data.dta")
-dropout_age <- read_dta("data/raw_data/dropout_age_1980_2010.dta")
-maps_d <- read_dta("data/raw_data/maps_d.dta")
-Figures_5_6_data <- read_dta("data/raw_data/Figures_5_6_data.dta")
+figure1_data <- read_dta("data/raw_data/replication_data/Figure_1_data.dta")
+dropout_age <- read_dta("data/raw_data/replication_data/dropout_age_1980_2010.dta")
+maps_d <- read_dta("data/raw_data/replication_data/maps_d.dta")
+Figures_5_6_data <- read_dta("data/raw_data/replication_data/Figures_5_6_data.dta")
 
 #### Save data ####
-write_csv(figure1_data, "data/raw_data/figure_1_data.csv") 
-write_csv(dropout_age, "data/raw_data/dropout_age.csv") 
-write_csv(maps_d, "data/raw_data/maps_d.csv") 
-write_csv(Figures_5_6_data, "data/raw_data/Figures_5_6_data.csv") 
-
-
+write_csv(figure1_data, "data/raw_data/reproduction_data/figure_1_data.csv") 
+write_csv(dropout_age, "data/raw_data/reproduction_data/dropout_age.csv") 
+write_csv(maps_d, "data/raw_data/reproduction_data/maps_d.csv") 
+write_csv(Figures_5_6_data, "data/raw_data/reproduction_data/Figures_5_6_data.csv") 
